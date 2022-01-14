b2c924196bdd0a21ad7ed917bac1bfaea18247c9
amandaelfassy1 <61420264+amandaelfassy1@users.noreply.github.com>
2022-01-14 14:38:40 +0100

    add get post

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use App\Http\Controllers\LikesController;
 // });
 
 Route::middleware('auth:sanctum')->get('auth/posts', [PostsController::class, 'index']);
-// Route::middleware('auth:sanctum')->get('auth/post', [PostsController::class, 'show']);
+Route::middleware('auth:sanctum')->get('auth/post', [PostsController::class, 'show']);
 Route::middleware('auth:sanctum')->post('auth/post', [PostsController::class, 'create']);
 Route::middleware('auth:sanctum')->post('auth/post/comment/{id}', [CommentsController::class, 'create']);
 Route::middleware('auth:sanctum')->delete('auth/post/delete/{id}', [PostsController::class, 'delete']);
