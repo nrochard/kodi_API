8a75a4118cc66c1e82a65708d10c2ccf83a5af56
Noémie ROCHARD <nrochard@student.42.fr>
2022-01-14 08:53:04 +0100

    modification migrations

--- database/migrations/2022_01_14_071847_create_likes_table.php
+++ database/migrations/2022_01_14_071847_create_likes_table.php
@@ -13,7 +13,7 @@ class CreateLikesTable extends Migration
      */
     public function up()
     {
-        Schema::create('liked', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->foreignId('post_id')
